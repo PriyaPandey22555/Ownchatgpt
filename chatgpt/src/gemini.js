3788fc426a527310afa4660d64501d1c5ded0405
PriyaPandey22555 <pp1549660@gmail.com>
2026-03-28 17:00:30 +0530

    updated code

--- chatgpt/src/gemini.js
+++ chatgpt/src/gemini.js
@@ -1,35 +1,28 @@
-const api="AIzaSyCFMrQ_aErGA4S5Htn-0vArCHSpXEz9Gk8"
+import { GoogleGenerativeAI } from "@google/generative-ai"
 
-import {
-    GoogleGenerativeAI,
-    HarmCategory,
-    HarmBlockThreshold,
-  } from "@google/generative-ai"
-  
+const api = import.meta.env.VITE_GEMINI_API_KEY;
 const genAI = new GoogleGenerativeAI(api);
-  
-  const model = genAI.getGenerativeModel({
-     model: "gemini-3-pro-preview",
-  });
-  
-  const generationConfig = {
+
+const model = genAI.getGenerativeModel({
+   model: "gemini-2.5-flash",
+});
+
+const generationConfig = {
     temperature: 1,
     topP: 0.95,
     topK: 40,
     maxOutputTokens: 8192,
     responseMimeType: "text/plain",
-  };
-  
-  async function run(prompt) {
+};
+
+async function run(prompt) {
     const chatSession = model.startChat({
-      generationConfig,
-      history: [
-      ],
+        generationConfig,
+        history: [],
     });
-  
+
     const result = await chatSession.sendMessage(prompt);
-    console.log(result.response.text());
-    return result.response.text()
-  }
-  
-  export default run;
+    return result.response.text();
+}
+
+export default run;
